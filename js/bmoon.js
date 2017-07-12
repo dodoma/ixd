8f8cf1cead052a748cd7237cab0116d210223dad
dodoma <bigml@qq.com>
2017-07-12 17:40:20 +0800

    js - 增加 bmoon.utl.clone() 用来拷贝对象

--- js/bmoon.js
+++ js/bmoon.js
@@ -59,6 +59,40 @@ bmoon.utl = {
         }
     },
 
+    clone: function(obj) {
+        var copy;
+
+        // Handle the 3 simple types, and null or undefined
+        if (null == obj || "object" != typeof obj) return obj;
+
+        // Handle Date
+        if (obj instanceof Date) {
+            copy = new Date();
+            copy.setTime(obj.getTime());
+            return copy;
+        }
+
+        // Handle Array
+        if (obj instanceof Array) {
+            copy = [];
+            for (var i = 0, len = obj.length; i < len; i++) {
+                copy[i] = bmoon.utl.clone(obj[i]);
+            }
+            return copy;
+        }
+
+        // Handle Object
+        if (obj instanceof Object) {
+            copy = {};
+            for (var attr in obj) {
+                if (obj.hasOwnProperty(attr)) copy[attr] = bmoon.utl.clone(obj[attr]);
+            }
+            return copy;
+        }
+
+        throw new Error("Unable to copy obj! Its type isn't supported.");
+    },
+
     stripHTML: function(string) {
         //return string.replace(/&/g,'&amp;').replace(/>/g,'&gt;').replace(/</g,'&lt;').replace(/\"/g,'&quot;');
         return string.replace(/>/g,'&gt;').replace(/</g,'&lt;').replace(/\"/g,'&quot;');

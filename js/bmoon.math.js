2c3947d050a4c7b593a24d38d90a3a81d0208136
dodoma <bigml@qq.com>
2017-07-14 11:33:19 +0800

    js - math process origin, scale

--- js/bmoon.math.js
+++ js/bmoon.math.js
@@ -12,16 +12,24 @@
 
     var abs = Math.abs;
 
-    function calculateRectangle(pointa, pointb) {
+    function calculateRectangle(origin, scale, pointa, pointb) {
+        var pa = [(pointa[0] - origin.x) / scale.x, (pointa[1] - origin.y) / scale.y],
+            pb = [(pointb[0] - origin.x) / scale.x, (pointb[1] - origin.y) / scale.y];
+
         return {
-            x: _min(pointa[0], pointb[0]),
-            y: _min(pointa[1], pointb[1]),
-            w: abs(pointa[0] - pointb[0]),
-            h: abs(pointa[1] - pointb[1])
+            x: _min(pa[0], pb[0]),
+            y: _min(pa[1], pb[1]),
+            w: abs(pa[0] - pb[0]),
+            h: abs(pa[1] - pb[1])
         }
     }
 
+    function actualPoint(origin, scale, point) {
+        return [(point[0] - origin.x) / scale.x, (point[1] - origin.y) / scale.y];
+    }
+
     _mmath = {
-        calculateRectangle: calculateRectangle
+        calculateRectangle: calculateRectangle,
+        actualPoint: actualPoint
     };
 })(jQuery);
